2b513338991887d1f5f0030997376ad3fec1e45e
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-08-21 19:51:42 -0500

    Inicio del proyecto, fachada del formulario creado y solo se importan los using para conexion SQL

---
+++ PruebaConexion/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace PruebaConexion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdBtnRegistrar = new System.Windows.Forms.RadioButton();
+            this.rdBtnEliminar = new System.Windows.Forms.RadioButton();
+            this.rdBtnModificar = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxId = new System.Windows.Forms.TextBox();
+            this.tbxNombre = new System.Windows.Forms.TextBox();
+            this.tbxApellido = new System.Windows.Forms.TextBox();
+            this.dtFechaNacim = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 300);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(542, 214);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdBtnModificar);
+            this.groupBox1.Controls.Add(this.rdBtnEliminar);
+            this.groupBox1.Controls.Add(this.rdBtnRegistrar);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(26, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(542, 84);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CRUD C# - SQL Server";
+            // 
+            // rdBtnRegistrar
+            // 
+            this.rdBtnRegistrar.AutoSize = true;
+            this.rdBtnRegistrar.Location = new System.Drawing.Point(55, 35);
+            this.rdBtnRegistrar.Name = "rdBtnRegistrar";
+            this.rdBtnRegistrar.Size = new System.Drawing.Size(93, 21);
+            this.rdBtnRegistrar.TabIndex = 0;
+            this.rdBtnRegistrar.TabStop = true;
+            this.rdBtnRegistrar.Text = "Registrar";
+            this.rdBtnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnEliminar
+            // 
+            this.rdBtnEliminar.AutoSize = true;
+            this.rdBtnEliminar.Location = new System.Drawing.Point(218, 35);
+            this.rdBtnEliminar.Name = "rdBtnEliminar";
+            this.rdBtnEliminar.Size = new System.Drawing.Size(84, 21);
+            this.rdBtnEliminar.TabIndex = 1;
+            this.rdBtnEliminar.TabStop = true;
+            this.rdBtnEliminar.Text = "Eliminar";
+            this.rdBtnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnModificar
+            // 
+            this.rdBtnModificar.AutoSize = true;
+            this.rdBtnModificar.Location = new System.Drawing.Point(381, 35);
+            this.rdBtnModificar.Name = "rdBtnModificar";
+            this.rdBtnModificar.Size = new System.Drawing.Size(92, 21);
+            this.rdBtnModificar.TabIndex = 2;
+            this.rdBtnModificar.TabStop = true;
+            this.rdBtnModificar.Text = "Modificar";
+            this.rdBtnModificar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Id:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Apellidos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Fecha de Nacimiento:";
+            // 
+            // tbxId
+            // 
+            this.tbxId.Location = new System.Drawing.Point(180, 129);
+            this.tbxId.Name = "tbxId";
+            this.tbxId.Size = new System.Drawing.Size(233, 20);
+            this.tbxId.TabIndex = 6;
+            // 
+            // tbxNombre
+            // 
+            this.tbxNombre.Location = new System.Drawing.Point(180, 170);
+            this.tbxNombre.Name = "tbxNombre";
+            this.tbxNombre.Size = new System.Drawing.Size(233, 20);
+            this.tbxNombre.TabIndex = 7;
+            // 
+            // tbxApellido
+            // 
+            this.tbxApellido.Location = new System.Drawing.Point(180, 211);
+            this.tbxApellido.Name = "tbxApellido";
+            this.tbxApellido.Size = new System.Drawing.Size(233, 20);
+            this.tbxApellido.TabIndex = 8;
+            // 
+            // dtFechaNacim
+            // 
+            this.dtFechaNacim.Location = new System.Drawing.Point(180, 252);
+            this.dtFechaNacim.Name = "dtFechaNacim";
+            this.dtFechaNacim.Size = new System.Drawing.Size(233, 20);
+            this.dtFechaNacim.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(482, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(482, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Modificar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(592, 532);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dtFechaNacim);
+            this.Controls.Add(this.tbxApellido);
+            this.Controls.Add(this.tbxNombre);
+            this.Controls.Add(this.tbxId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Conexion SQL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdBtnModificar;
+        private System.Windows.Forms.RadioButton rdBtnEliminar;
+        private System.Windows.Forms.RadioButton rdBtnRegistrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxId;
+        private System.Windows.Forms.TextBox tbxNombre;
+        private System.Windows.Forms.TextBox tbxApellido;
+        private System.Windows.Forms.DateTimePicker dtFechaNacim;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
